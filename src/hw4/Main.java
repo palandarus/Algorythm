beeb57a67d02570ad0b24c7899f02346ab8b7511
palandarus <klarkken@yandex.ru>
2020-07-07 15:24:09 +0300

    initial commit

---
+++ src/hw4/Main.java
@@ -0,0 +1,29 @@
+package hw4;
+
+/**
+ * 1. Реализовать класс итератора для двусвязного списка.
+ * 2. Реализовать двусвязный список
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+        RelatedList rl = new RelatedList();
+        rl.push(new Cat(2, "cat1"));
+        rl.push(new Cat(3, "cat2"));
+        rl.push(new Cat(4, "cat3"));
+        rl.push(new Cat(5, "cat4"));
+        rl.push(new Cat(6, "cat5"));
+        System.out.println(rl);
+        System.out.println(rl.pop());
+        System.out.println(rl);
+    }
+    //reset();
+    //next();
+    //getCurrent();
+    //hasNext();
+    //atEnd();
+    //insertAfter();
+    //*insertBefore();
+    //deleteCurrent();
+
+}
