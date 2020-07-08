105da2307c2639ff62bf075f4911d0336ed8180c
palandarus <klarkken@yandex.ru>
2020-07-08 17:20:23 +0300

    добавил итератор

--- src/hw4/Main.java
+++ src/hw4/Main.java
@@ -7,23 +7,29 @@ package hw4;
 
 public class Main {
     public static void main(String[] args) {
-        RelatedList rl = new RelatedList();
-        rl.push(new Cat(2, "cat1"));
-        rl.push(new Cat(3, "cat2"));
-        rl.push(new Cat(4, "cat3"));
-        rl.push(new Cat(5, "cat4"));
-        rl.push(new Cat(6, "cat5"));
-        System.out.println(rl);
-        System.out.println(rl.pop());
-        System.out.println(rl);
+        TwoSidesRelatedList drl = new TwoSidesRelatedList();
+        drl.push(new Cat(1, "cat1"));
+        drl.push(new Cat(2, "cat2"));
+        drl.push(new Cat(3, "cat3"));
+        System.out.println(drl);
+        drl.pop();
+        System.out.println(drl);
+        System.out.println(drl.getIter().getCurrent());
+        System.out.println(drl.getIter().hasNext());
+        //System.out.println(drl.getIter().next());
+        System.out.println(drl.getIter().getCurrent());
+        drl.getIter().insertBefore(new Cat(5,"cat5"));
+        System.out.println(drl);
+        drl.getIter().insertAfter(new Cat(6,"cat6"));
+        System.out.println(drl);
+        System.out.println(drl.getIter().getCurrent());
+        drl.getIter().deleteCurrent();
+        System.out.println(drl);
+        drl.push(new Cat(4, "cat4"));
+        System.out.println(drl);
+        drl.delete(new Cat(4, "cat4"));
+        System.out.println(drl);
     }
-    //reset();
-    //next();
-    //getCurrent();
-    //hasNext();
-    //atEnd();
-    //insertAfter();
-    //*insertBefore();
-    //deleteCurrent();
+
 
 }
--- src/hw4/TwoSidesRelatedList.java
+++ src/hw4/TwoSidesRelatedList.java
@@ -1,8 +1,11 @@
 package hw4;
 
+
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class TwoSidesRelatedList {
+
     private class Node {
         Cat c;
         Node next;
@@ -30,13 +33,118 @@ public class TwoSidesRelatedList {
             return Objects.hash(c);
         }
     }
+    public class Iter{
+        Node iterator;
+
+        //reset();
+
+        public void reset() {
+            iterator = head;
+        }
+
+        //next();
+
+        public Node next() {
+            if (atEnd()) throw new NoSuchElementException();
+            iterator = iterator.next;
+            return iterator;
+        }
+
+        //getCurrent();
+
+        public Node getCurrent() {
+            return iterator;
+        }
+
+        //hasNext();
+        public boolean hasNext() {
+            return !atEnd();
+        }
+
+        //atEnd();
+        public boolean atEnd() {
+            if (iterator.next == null) return true;
+            else return false;
+        }
+
+
+        public boolean atBegin() {
+            return false;
+        }
+
+        //insertAfter();
+        public void insertAfter(Cat cat) {
+            Node node=new Node(cat);
+            if (!atEnd()) {
+                iterator.next.prev = node;
+                node.next = iterator.next;
+            }
+            iterator.next = node;
+            node.prev = iterator;
+            size++;
+
+        }
+
+        //*insertBefore();
+        public void insertBefore(Cat cat) {
+            Node node=new Node(cat);
+            node.next = iterator;
+            if (iterator.prev == null) {
+                node.prev = null;
+                head=node;
+            }
+            else {
+                node.prev=iterator.prev;
+                iterator.prev.next=node;
+            }
+            iterator.prev=node;
+            size++;
+        }
+
+
+        //deleteCurrent();
+        public Cat deleteCurrent(){
+            Node temp=iterator;
+            if (iterator==null) throw new NoSuchElementException();
+            if(atEnd()){
+                if (iterator.prev==null){
+                    head=null;
+                    size--;
+                    reset();
+                }
+                else{
+                    iterator=iterator.prev;
+                    iterator.next=null;
+                    size--;
+                }
+            }
+            else {
+                if(iterator.prev==null){
+                    head=iterator.next;
+                    head.prev=null;
+                    reset();
+                    size--;
+                }
+                else{
+                    iterator.prev.next=iterator.next;
+                    iterator.next.prev=iterator.prev;
+                    size--;
+                }
+            }
+            return temp.c;
+        }
+    }
+
 
     private Node head;
     private int size;
+    private Iter iter;
 
     public TwoSidesRelatedList() {
         head = null;
         size = 0;
+        iter=new Iter();
+        iter.reset();
     }
 
     public boolean isEmpty() {
@@ -45,19 +153,25 @@ public class TwoSidesRelatedList {
 
     public void push(Cat c) {
         Node n = new Node(c);
-        head.prev=n;
+        if (!isEmpty()) head.prev = n;
         n.next = head;
         head = n;
         size++;
+        iter.reset();
     }
 
     public Cat pop() {
         if (isEmpty()) return null;
         Cat temp = head.c;
         head = head.next;
-        head.prev=null;
+        if (head.next != null) head.prev = null;
         size--;
+        iter.reset();
         return temp;
+    }
+
+    public Iter getIter() {
+        return iter;
     }
 
     @Override
@@ -101,9 +215,12 @@ public class TwoSidesRelatedList {
         }
         if (current == head) {
             head = head.next;
+            head.prev = null;
         } else {
             previous.next = current.next;
+            previous.next.prev = previous;
         }
+        iter.reset();
         return true;
     }
 }
